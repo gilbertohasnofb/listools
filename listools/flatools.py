54c3c320f8fd22b5e20dc45c247cb49923bd6358
gilbertohasnofb <gilbertohasnofb@gmail.com>
2018-12-17 13:48:42 +0000

    improving code comment

--- listools/flatools.py
+++ listools/flatools.py
@@ -445,8 +445,8 @@ def flatten_max(input_list: list,
     if not isinstance(input_list, list):
         raise TypeError('\'input_list\' must be \'list\'')
     # not very elegant, but max(input, key=None) raises "TypeError: 'NoneType'
-    # object is not callable". Meanwhile, sorted(input, key=None) works exactly
-    # as expected.
+    # object is not callable. Other functions such as sorted(input, key=None)
+    # works exactly as expected when key=None.
     if key:
         return max(flatten(input_list), key=key, default=default)
     else:
