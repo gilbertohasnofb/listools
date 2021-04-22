c57c9842f4aca974f755e0300865dd8973d23c1a
jgart <47760695+jgarte@users.noreply.github.com>
2021-04-22 17:15:04 -0400

    Same suggestion as the one I made for auxjad

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ release = listools.__version__
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
